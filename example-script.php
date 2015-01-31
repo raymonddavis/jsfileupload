7436d6e7e1b84847b6adc6d970bb67908e805545
raymonddavis <raymond.davis.4th@gmail.com>
2015-01-31 01:39:36 -0600

    for pulling

--- example-script.php
+++ example-script.php
@@ -1,4 +1,12 @@
 <?php
-unlink($_FILES['file']['tmp_name']);
-echo $_FILES['file']['name'];
+header('Content-Type: application/json');
+
+$des = '/var/www/git/jsfileupload/uploads/';
+
+move_uploaded_file($_FILES['file']['tmp_name'], $des.$_FILES['file']['name']);
+
+if (file_exists($des.$_FILES['file']['name']))
+	echo json_encode($_FILES['file']['name']);
+else
+	echo json_encode('Failed');
 ?>
